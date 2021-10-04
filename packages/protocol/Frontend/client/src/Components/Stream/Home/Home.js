16b184715832bfddf7769cb87f706b958c1d5863
yeatun <p.yeatun@gmail.com>
2021-10-04 15:49:15 +0600

    designed stream part

---
+++ packages/protocol/Frontend/client/src/Components/Stream/Home/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import FrontPage from '../../FrontPage/FrontPage';
+
+import Navbar from './../../Navbar/Navbar';
+
+const Home = () => {
+    return (
+        <div className=''>
+          
+            <Navbar></Navbar>
+           <FrontPage></FrontPage>
+           
+        </div>
+    );
+};
+
+export default Home;
