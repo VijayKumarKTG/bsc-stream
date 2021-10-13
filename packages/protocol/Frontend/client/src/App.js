95dcb9ccf3f6c7856575b7ec2e750b3966dbf9de
VijayKumarKTG <vijaykumardevktg@gmail.com>
2021-10-13 20:32:57 +0630

    Add chain network detection

--- packages/protocol/Frontend/client/src/App.js
+++ packages/protocol/Frontend/client/src/App.js
@@ -3,11 +3,23 @@ import Home from './Components/Stream/Home/Home';
 import Dashboard from './Components/Dashboard/Dashboard';
 import Stream from './Components/Stream/Stream/Stream';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { AccountContext } from './context/AccountContext';
 
 function App() {
   const [account, setAccount] = useState('');
+  const [chainId, setChainId] = useState('');
+
+  useEffect(() => {
+    const setChain = async () => {
+      const chainID = await window.ethereum.request({ method: 'eth_chainId' });
+      console.log(chainID);
+      if (chainID === '0x61' || chainID === '97') {
+        setChainId(chainID);
+      }
+    };
+    setChain();
+  }, []);
 
   const changeAccount = (newAccount) => {
     setAccount(newAccount);
@@ -29,9 +41,53 @@ function App() {
     }
   });
 
+  window.ethereum.on('chainChanged', (chainID) => {
+    window.location.reload();
+  });
+
+  const changeChain = () => {
+    window.ethereum
+      .request({
+        method: 'wallet_addEthereumChain',
+        params: [
+          {
+            chainId: '0x61',
+            chainName: 'BSC Testnet',
+            nativeCurrency: {
+              name: 'Binance Coin',
+              symbol: 'BNB',
+              decimals: 18,
+            },
+            rpcUrls: ['https://data-seed-prebsc-1-s1.binance.org:8545/'],
+          },
+        ],
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <AccountContext.Provider value={contextValue}>
       <div className='App-header'>
+        {chainId !== '0x61' && (
+          <div
+            style={{
+              position: 'absolute',
+              top: '0',
+              left: '0',
+              right: '0',
+              width: '100%',
+              zIndex: '10',
+            }}>
+            Please change your blockchain network to{' '}
+            <span
+              style={{ color: 'blue', cursor: 'pointer', fontWeight: 'bold' }}
+              onClick={changeChain}>
+              BSC Testnet
+            </span>
+          </div>
+        )}
         <Router>
           <Switch>
             <Route path='/stream'>
